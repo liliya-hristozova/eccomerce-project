37c5f8a4775b32b8b6ae941c923bbb2c0a9b3b4f
Hristozova <liliya.hristozova@accenture.com>
2020-02-13 15:46:58 +0200

    add data file

--- src/data.js
+++ src/data.js
@@ -42,21 +42,21 @@ export default [
       image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/68240d6a81a5476f986aaa8500aa61bc_9366/ozweego-shoes.jpg"
     },
     {
-        id: '4',
+        id: '7',
         title: "Ultraboost Shoes",
         price: 139.95,
         brand: "Running",
         image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/dd81e838b0b643b2bb81a93f012ef105_9366/ultraboost-shoes.jpg"
       },
       {
-        id: '5',
+        id: '8',
         title: "NMD_R1 Shoes",
         price: 109.95,
         brand: "Originals",
         image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/801e88f5897a411a9e77a8ba0101c79a_9366/nmd_r1-shoes.jpg"
       },
       {
-        id: '6',
+        id: '9',
         title: "Ozweego Shoes",
         price: 89.95,
         brand: "Originals",
