d6e40f7916674bf162e90f573706581d8b245596
Liliya Hristozova <lilia.hristozova@gmail.com>
2020-02-16 23:56:06 +0200

    add raiting in data file

--- src/data.js
+++ src/data.js
@@ -4,62 +4,71 @@ export default [
       title: "Supercourt Shoes",
       price: 69.95,
       brand: "Originals",
-      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/710d628d52f0418eb331aacb00e3c33a_9366/supercourt-shoes.jpg"
+      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/710d628d52f0418eb331aacb00e3c33a_9366/supercourt-shoes.jpg",
+      raiting: 4
     },
     {
       id: '2',
       title: "Stan Smith Shoes",
       price: 74.95,
       brand: "Originals",
-      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/25c70a990dd74210aa47a59900ebfe5d_9366/stan-smith-shoes.jpg"
+      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/25c70a990dd74210aa47a59900ebfe5d_9366/stan-smith-shoes.jpg",
+      raiting: 3
     },
     {
       id: '3',
       title: "Supercourt Shoes",
       price: 69.95,
       brand: "Originals",
-      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/7d48caa16daf414a9aa2aaa200be951d_9366/supercourt-shoes.jpg"
+      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/7d48caa16daf414a9aa2aaa200be951d_9366/supercourt-shoes.jpg",
+      raiting: 2
     },
     {
       id: '4',
       title: "Ultraboost Shoes",
       price: 139.95,
       brand: "Running",
-      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/dd81e838b0b643b2bb81a93f012ef105_9366/ultraboost-shoes.jpg"
+      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/dd81e838b0b643b2bb81a93f012ef105_9366/ultraboost-shoes.jpg",
+      raiting: 5
     },
     {
       id: '5',
       title: "NMD_R1 Shoes",
       price: 109.95,
       brand: "Originals",
-      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/801e88f5897a411a9e77a8ba0101c79a_9366/nmd_r1-shoes.jpg"
+      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/801e88f5897a411a9e77a8ba0101c79a_9366/nmd_r1-shoes.jpg",
+      raiting: 4
     },
     {
       id: '6',
       title: "Ozweego Shoes",
       price: 89.95,
       brand: "Originals",
-      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/68240d6a81a5476f986aaa8500aa61bc_9366/ozweego-shoes.jpg"
+      image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/68240d6a81a5476f986aaa8500aa61bc_9366/ozweego-shoes.jpg",
+      raiting: 3
     },
     {
         id: '7',
         title: "Ultraboost Shoes",
         price: 139.95,
         brand: "Running",
-        image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/dd81e838b0b643b2bb81a93f012ef105_9366/ultraboost-shoes.jpg"
+        image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/dd81e838b0b643b2bb81a93f012ef105_9366/ultraboost-shoes.jpg",
+        raiting: 5
       },
       {
         id: '8',
         title: "NMD_R1 Shoes",
         price: 109.95,
         brand: "Originals",
-        image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/801e88f5897a411a9e77a8ba0101c79a_9366/nmd_r1-shoes.jpg"
+        image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/801e88f5897a411a9e77a8ba0101c79a_9366/nmd_r1-shoes.jpg",
+        raiting: 4
       },
       {
         id: '9',
         title: "Ozweego Shoes",
         price: 89.95,
         brand: "Originals",
-        image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/68240d6a81a5476f986aaa8500aa61bc_9366/ozweego-shoes.jpg"
+        image: "https://assets.adidas.com/images/w_385,h_385,f_auto,q_auto:sensitive,fl_lossy/68240d6a81a5476f986aaa8500aa61bc_9366/ozweego-shoes.jpg",
+        raiting: 2
       },
   ]
