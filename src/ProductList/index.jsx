53a62a06ed2ee8b8b84f180a982fd01689f4fe99
Liliya Hristozova <lilia.hristozova@gmail.com>
2020-02-16 23:58:43 +0200

    add Product in to ProductList

--- src/ProductList/index.jsx
+++ src/ProductList/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Product from '../Product/product';
+import Product from '../Product/index';
+import productsData from '../data';
 
 const ProductList = () => (
     <div className="row">
-        <Product />
+        { productsData.map((productData) => (
+            <Product product={productData} key={productData.id}/>
+        ))}
     </div>
-    
 );
 
 export default ProductList;
