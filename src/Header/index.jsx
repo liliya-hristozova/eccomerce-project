42cde8e213f514a5a521f5c0a5a7321fbe62d549
Liliya Hristozova <lilia.hristozova@gmail.com>
2020-02-16 23:57:04 +0200

    add header cart link

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link, BrowserRouter} from 'react-router-dom';
 import './header.css';
 
 const Header = () => (
-    <div className="header bg-dark">
+    <div className="header navbar navbar-dark bg-dark shadow-sm">
+        <div className="container d-flex justify-content-between">
+        <BrowserRouter>
+            <Link className="navbar-brand d-flex align-items-center" to={'/'}>Shop</Link>
+            <Link className="shopping-cart-button btn btn-outline-secondary my-2" to='/cart'>
+               Shopping Cart
+            </Link>
+            
+        </BrowserRouter>
+        </div>
     </div>
     
 );
